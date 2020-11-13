28a141381fbff514db1970242ab24e687907ee83
z-spider <zsqxby@gmail.com>
2020-11-13 11:10:49 +0800

    fix bug about XSB pattern error

--- B易收银数据处理.bat
+++ B易收银数据处理.bat
@@ -1,3 +1,4 @@
+call A数据格式化.bat
 echo "YSY_____________%time%-|-%date%_____________YSY">>run.log
 perl .\script\data2deal.pl 2>>run.log
 copy data\易收银日激活.txt "data\OldData\YSYactivation_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt" 2>>run.log
